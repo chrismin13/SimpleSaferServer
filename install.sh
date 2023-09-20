f9ee6d7aa19ca66c39475acf87b9f38341079033
Christos Miniotis <accounts@chrismin13.com>
2023-09-21 02:36:15 +0300

    try something

--- install.sh
+++ install.sh
@@ -15,10 +15,6 @@ config_path="/etc/SimpleSaferServer/config.conf"
 # Create config directory if it doesn't exist
 mkdir -p $(dirname "$config_path")
 
-# Load existing configurations
-if [ -f "$config_path" ]; then
-  source $config_path
-fi
 
 # Prompt for missing variables
 declare -A prompts=(
@@ -29,6 +25,15 @@ declare -A prompts=(
     ["MOUNT_POINT"]="Enter the mount point of the external hard drive (e.g. /media/backup): "
     ["RCLONE_DIR"]="Enter the backup directory on Rclone (e.g. OneDriveCrypt:): "
 )
+
+for key in "${!prompts[@]}"; do
+  eval $key=
+done
+
+# Load existing configurations
+if [ -f "$config_path" ]; then
+  source $config_path
+fi
 
 for key in "${!prompts[@]}"; do
     if [ -z "${!key}" ]; then
