dec6c4b596285f36112193db454d8d6df48c702d
Christos Miniotis <accounts@chrismin13.com>
2023-09-21 20:35:26 +0300

    Add bw_limit

--- install.sh
+++ install.sh
@@ -22,23 +22,24 @@ fi
 
 # Prompt for missing variables
 declare -A prompts=(
-    ["EMAIL_ADDRESS"]="Enter the email address for alerts: "
-    ["SERVER_NAME"]="Enter the server name: "
-    ["UUID"]="Enter the UUID of the external hard drive (e.g. 2CD49023D48FED80): "
-    ["USB_ID"]="Enter the USB ID of the external hard drive (e.g. 0480:a202): "
-    ["MOUNT_POINT"]="Enter the mount point of the external hard drive (e.g. /media/backup): "
-    ["RCLONE_DIR"]="Enter the backup directory on Rclone (e.g. OneDriveCrypt:): "
+  ["EMAIL_ADDRESS"]="Enter the email address for alerts: "
+  ["SERVER_NAME"]="Enter the server name: "
+  ["UUID"]="Enter the UUID of the external hard drive (e.g. 2CD49023D48FED80): "
+  ["USB_ID"]="Enter the USB ID of the external hard drive (e.g. 0480:a202): "
+  ["MOUNT_POINT"]="Enter the mount point of the external hard drive (e.g. /media/backup): "
+  ["RCLONE_DIR"]="Enter the backup directory on Rclone (e.g. OneDriveCrypt:): "
+  ["BW_LIMIT"]="Optional: Enter a bandwidth limit for the rclone backup (e.g. 4M): "
 )
 
 for key in "${!prompts[@]}"; do
-    if [ -z "${!key}" ]; then
-        read -p "${prompts[$key]}" input
-        eval $key='$input'
-    fi
+  if [ -z "${!key}" ]; then
+    read -p "${prompts[$key]}" input
+    eval $key='$input'
+  fi
 done
 
 # Update or create config file
-cat <<EOL > "$config_path"
+cat <<EOL >"$config_path"
 USERNAME="$invoked_user"
 EMAIL_ADDRESS="$EMAIL_ADDRESS"
 SERVER_NAME="$SERVER_NAME"
@@ -46,6 +47,7 @@ UUID="$UUID"
 USB_ID="$USB_ID"
 MOUNT_POINT="$MOUNT_POINT"
 RCLONE_DIR="$RCLONE_DIR"
+BW_LIMIT="$BW_LIMIT"
 EOL
 
 # TODO: Add a menu for script selection here
@@ -59,7 +61,7 @@ chmod +x /usr/local/bin/*.sh
 
 # Modify .service files to include the proper username and group, then copy them to /etc/systemd/system/
 for f in ./services/*.service; do
-    sed -e "s/__USER__/$invoked_user/g" -e "s/__GROUP__/$invoked_user/g" "$f" > "/etc/systemd/system/$(basename $f)"
+  sed -e "s/__USER__/$invoked_user/g" -e "s/__GROUP__/$invoked_user/g" "$f" >"/etc/systemd/system/$(basename $f)"
 done
 
 # Copy systemd timer files
--- scripts/backup_cloud.sh
+++ scripts/backup_cloud.sh
@@ -16,13 +16,21 @@ else
   exit 1
 fi
 
-rclone sync "$MOUNT_POINT" "$RCLONE_DIR" --create-empty-src-dirs
+# Check if there's a bandwith limit
+if [ -n "$BW_LIMIT" ]; then
+  $extra_args="--bw-limit $BW_LIMIT" # e.g. --bw-limit 4M will limit the speed to 4mbps
+fi
+
+# Run rclone. The arguments are:
+# --create-empty-src-dirs: Copies empty folders
+# -v: Keeps a log of all the changes
+# $extra_args: Whatever was set above
+rclone sync "$MOUNT_POINT" "$RCLONE_DIR" --create-empty-src-dirs -v $extra_args
 
 ## send email in case of error ##
-if [ $? -ne 0 ]
-then
+if [ $? -ne 0 ]; then
   logs=$(journalctl -u backup-cloud.service -n 100)
   echo "ERROR: BACKUP FAILED"
-  echo -e "Subject: BACKUP TO CLOUD FAILED - Unknown Error - $SERVER_NAME\n$logs"| msmtp $EMAIL_ADDRESS
+  echo -e "Subject: BACKUP TO CLOUD FAILED - Unknown Error - $SERVER_NAME\n$logs" | msmtp $EMAIL_ADDRESS
   exit 1
 fi
