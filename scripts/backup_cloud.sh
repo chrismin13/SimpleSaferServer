f0a1b8ffb6f491a2af3bcf14a0893b8a1c782ff7
Christos Miniotis <accounts@chrismin13.com>
2023-09-21 21:09:12 +0300

    Add checks for IO errors

--- scripts/backup_cloud.sh
+++ scripts/backup_cloud.sh
@@ -16,6 +16,15 @@ else
   exit 1
 fi
 
+# Check for I/O Errors
+if ls $MOUNT_POINT; then
+  echo "No IO Errors have occured"
+else
+  echo "ERROR: DRIVE HAS IO ERRORS"
+  echo -e "Subject: BACKUP TO CLOUD FAILED - Drive has IO Errors - $SERVER_NAME\nCheck the connection to the Hard Drive!" | msmtp $EMAIL_ADDRESS
+  exit 1
+fi
+
 # Check if there's a bandwith limit
 if [ -n "$BANDWIDTH_LIMIT" ]; then
   extra_args="--bwlimit $BANDWIDTH_LIMIT" # e.g. --bwlimit 4M will limit the speed to 4mbps
@@ -29,7 +38,7 @@ rclone sync "$MOUNT_POINT" "$RCLONE_DIR" --create-empty-src-dirs -v $extra_args
 
 ## send email in case of error ##
 if [ $? -ne 0 ]; then
-  logs=$(journalctl -u backup-cloud.service -n 100)
+  logs=$(journalctl -u backup_cloud.service -n 100)
   echo "ERROR: BACKUP FAILED"
   echo -e "Subject: BACKUP TO CLOUD FAILED - Unknown Error - $SERVER_NAME\n$logs" | msmtp $EMAIL_ADDRESS
   exit 1
--- scripts/check_mount.sh
+++ scripts/check_mount.sh
@@ -35,5 +35,22 @@ else
     echo "Device is mounted"
 fi
 
+# Check for I/O Errors
+if ! ls $MOUNT_POINT; then
+    # Attempt to unmount the device first
+    if ! umount $MOUNT_POINT; then
+        send_email "Failed to unmount device after IO Errors occured." "IO Error - Failed to unmount"
+    fi
+    if ! mount /dev/disk/by-uuid/$UUID; then
+        # If mounting failed, send an email
+        send_email "Failed to mount device with UUID $UUID at $MOUNT_POINT after IO Errors had occured." "Mounting Error due to IO Errors"
+        exit 1
+    else
+        send_email "Device with UUID $UUID at $MOUNT_POINT was remounted after IO Errors had occured." "Device Remounted due to IO Errors"
+    fi
+else
+    echo "No IO Errors have occured"
+fi
+
 # If all checks passed, then exit with success
 exit 0
