d2be45382331ab3fb03a876c1a7fd64e2c40a7fd
Christos Miniotis <accounts@chrismin13.com>
2024-03-30 01:03:47 +0200

    Change how variables are stored to print them in order

--- install.sh
+++ install.sh
@@ -20,27 +20,32 @@ if [ -f "$config_path" ]; then
   source $config_path
 fi
 
-# Prompt for missing variables
-declare -A prompts=(
-  ["EMAIL_ADDRESS"]="Enter the email address for alerts: "
-  ["SERVER_NAME"]="Enter the server name: "
-  ["UUID"]="Enter the UUID of the external hard drive (e.g. 2CD49023D48FED80): "
-  ["USB_ID"]="Enter the USB ID of the external hard drive (e.g. 0480:a202): "
-  ["MOUNT_POINT"]="Enter the mount point of the external hard drive (e.g. /media/backup): "
-  ["RCLONE_DIR"]="Enter the backup directory on Rclone (e.g. OneDriveCrypt:): "
-  ["BANDWIDTH_LIMIT"]="Optional: Enter a bandwidth limit for the rclone backup (e.g. 4M): "
-  ["CHECK_HDSENTINEL_HEALTH_TIME"]="You will now be asked to enter a time for the scheduled tasks. Use a time when the server and network won't be in use to avoid interruptions. Keep the tasks seperated from each other, e.g first at 2:49, next at 2:50 and final at 3:00.\n\nEnter the start time for checking HD Sentinel health (e.g. 2:49:00): "
-  ["CHECK_MOUNT_TIME"]="Enter the start time for checking if the disk is mounted (e.g. 2:50:00): "
-  ["BACKUP_CLOUD_TIME"]="Enter the start time for the cloud backup (e.g. 3:00:00): "
-
+# Define the order and prompts in separate arrays
+order=("EMAIL_ADDRESS" "SERVER_NAME" "UUID" "USB_ID" "MOUNT_POINT" "RCLONE_DIR" "BANDWIDTH_LIMIT" "CHECK_HDSENTINEL_HEALTH_TIME" "CHECK_MOUNT_TIME" "BACKUP_CLOUD_TIME")
+prompts=(
+  "Enter the email address for alerts: "
+  "Enter the server name: "
+  "Enter the UUID of the external hard drive (e.g. 2CD49023D48FED80): "
+  "Enter the USB ID of the external hard drive (e.g. 0480:a202): "
+  "Enter the mount point of the external hard drive (e.g. /media/backup): "
+  "Enter the backup directory on Rclone (e.g. OneDriveCrypt:): "
+  "Optional: Enter a bandwidth limit for the rclone backup (e.g. 4M): "
+  "You will now be asked to enter a time for the scheduled tasks. Use a time when the server and network won't be in use to avoid interruptions. Keep the tasks separated from each other, e.g first at 2:49, next at 2:50 and final at 3:00.\n\nEnter the start time for checking HD Sentinel health (e.g. 2:49:00): "
+  "Enter the start time for checking if the disk is mounted (e.g. 2:50:00): "
+  "Enter the start time for the cloud backup (e.g. 3:00:00): "
 )
 
-for key in "${!prompts[@]}"; do
-  if [ -z "${!key}" ]; then
-    read -p "${prompts[$key]}" input
+# Prompt for missing variables based on the order
+for i in "${!order[@]}"; do
+  key=${order[i]}
+  prompt=${prompts[i]}
+  current_value=$(eval echo \$$key)
+  if [ -z "$current_value" ]; then
+    read -p "$prompt" input
     eval $key='$input'
   fi
 done
+
 
 # Update or create config file
 cat <<EOL >"$config_path"
